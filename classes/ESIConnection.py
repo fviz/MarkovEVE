63d047c153c2389383844cd9e4a9a2b226608407
Francisco Vizentin <franciscovizentin@gmail.com>
2018-12-08 13:28:14 +0100

    clean up connection class

--- classes/ESIConnection.py
+++ classes/ESIConnection.py
@@ -17,7 +17,7 @@ class ESI:
 			redirect_uri="http://localhost/callback/",
 			client_id="5cc3f56b88364587b5b25aeb0299cdd1",
 			secret_key="HX9ed1EYpPQj1bnCklcWFXAborUvXXEpv8BQdEcg",
-			headers={'User-Agent': 'PythonProject'},
+			headers={'User-Agent': 'PythonProject'}
 		)
 
 		self.app = App.create(url="https://esi.tech.ccp.is/latest/swagger.json?datasource=tranquility")
